3eb24e269540b91cb2f32f3dec230c746113b40c
Mirko Augsburger <git+nike@mist.name>
2018-05-17 16:09:57 +0200

    Add Language menu to settings and parse it in StartActivity

--- FetLife/fetlife/src/main/java/com/bitlove/fetlife/StartActivity.java
+++ FetLife/fetlife/src/main/java/com/bitlove/fetlife/StartActivity.java
@@ -1,7 +1,11 @@
 package com.bitlove.fetlife;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 
 import com.bitlove.fetlife.inbound.onesignal.notification.OneSignalNotification;
 import com.bitlove.fetlife.session.UserSessionManager;
@@ -9,10 +13,14 @@ import com.bitlove.fetlife.view.screen.resource.ConversationsActivity;
 import com.bitlove.fetlife.view.screen.resource.FeedActivity;
 import com.bitlove.fetlife.view.screen.standalone.LoginActivity;
 
+import java.util.Locale;
+
 /**
  * Default Start Activity to make Activity title and App name independent
  */
 public class StartActivity extends Activity {
+
+    public static final String TAG = "StartActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,15 @@ public class StartActivity extends Activity {
         if (userSessionManager.getCurrentUser() == null) {
             LoginActivity.startLogin(getFetLifeApplication());
         } else {
+
+            String localeString = getFetLifeApplication().getUserSessionManager().getActiveUserPreferences().getString(getString(R.string.settings_key_general_language),null);
+            if (localeString != null && ! localeString.isEmpty()) {
+                Log.d(TAG, "onCreate: localeString = " + localeString + "; changing language");
+                this.setLocale(localeString);
+            } else {
+                Log.d(TAG, "onCreate: localeString = " + localeString);
+            }
+
             if (getFetLifeApplication().getUserSessionManager().getActiveUserPreferences().getBoolean(getString(R.string.settings_key_general_feed_as_start),false)) {
                 FeedActivity.startActivity(this);
             } else {
@@ -42,4 +59,13 @@ public class StartActivity extends Activity {
         return (FetLifeApplication) getApplication();
     }
 
+    private void setLocale(String localeString){
+        Context context = getApplicationContext();
+        Resources res = context.getResources();
+        // Change locale settings in the app.
+        DisplayMetrics dm = res.getDisplayMetrics();
+        android.content.res.Configuration conf = res.getConfiguration();
+        conf.locale = new Locale(localeString);
+        res.updateConfiguration(conf, dm);
+    }
 }
