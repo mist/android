92b7d52951d91b648f3a845b901c8148bd680cc2
kistitan <kistitan@users.noreply.github.com>
2018-06-18 12:34:14 +0200

    Temporary handling of expired token

--- FetLifeKotlin/app/src/main/java/com/bitlove/fetlife/model/network/job/get/GetListResourceJob.kt
+++ FetLifeKotlin/app/src/main/java/com/bitlove/fetlife/model/network/job/get/GetListResourceJob.kt
@@ -1,8 +1,10 @@
 package com.bitlove.fetlife.model.network.job.get
 
+import com.bitlove.fetlife.FetLifeApplication
 import com.bitlove.fetlife.model.dataobject.entity.content.DataEntity
 import com.bitlove.fetlife.model.db.FetLifeContentDatabase
 import com.bitlove.fetlife.model.network.job.BaseJob
+import com.bitlove.fetlife.view.login.LoginActivity
 import retrofit2.Call
 import retrofit2.Response
 
@@ -17,6 +19,11 @@ abstract class GetListResourceJob<T : DataEntity>(jobPriority: Int, doPersist: B
                 saveToDb(contentDb, getResultBody(result))
             },true)
             true
+        } else if (result.code() == 401) {
+            //TODO move this to general response handling
+            FetLifeApplication.instance.onUserLoggedOut()
+            LoginActivity.start(FetLifeApplication.instance)
+            false
         } else {
             false
         }
