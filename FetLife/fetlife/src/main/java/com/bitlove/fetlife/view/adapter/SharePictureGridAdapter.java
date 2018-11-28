394b17f8a505ebf6b1fc867de7ffc21f45231e7a
kistitan <kistitan@users.noreply.github.com>
2018-11-28 12:53:33 +0100

    Compile fix

--- FetLife/fetlife/src/main/java/com/bitlove/fetlife/view/adapter/SharePictureGridAdapter.java
+++ FetLife/fetlife/src/main/java/com/bitlove/fetlife/view/adapter/SharePictureGridAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import com.bitlove.fetlife.FetLifeApplication;
 import com.bitlove.fetlife.R;
 import com.bitlove.fetlife.model.pojos.fetlife.dbjson.Member;
 import com.bitlove.fetlife.model.pojos.fetlife.dbjson.Picture;
