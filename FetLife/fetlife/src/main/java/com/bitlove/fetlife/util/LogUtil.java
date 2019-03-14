65ff6adf6ee3b2935c467a7bb9598b3a94b1c286
kistitan <kistitan@users.noreply.github.com>
2019-03-14 19:59:20 +0100

    Memory logs

--- FetLife/fetlife/src/main/java/com/bitlove/fetlife/util/LogUtil.java
+++ FetLife/fetlife/src/main/java/com/bitlove/fetlife/util/LogUtil.java
@@ -1,5 +1,9 @@
 package com.bitlove.fetlife.util;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+
 import com.bitlove.fetlife.BuildConfig;
 import com.bitlove.fetlife.FetLifeApplication;
 import com.crashlytics.android.Crashlytics;
@@ -8,6 +12,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 
 public class LogUtil {
+
+    private static String localLog = "";
 
     public static void writeLog(String message) {
 
@@ -20,10 +26,25 @@ public class LogUtil {
             if (!file.exists()) file.createNewFile();
             FileOutputStream fos = new FileOutputStream(file,true);
             String log = DateUtil.toServerString(System.currentTimeMillis()) + " - " + message + "\n";
+            localLog += log;
             fos.write(log.getBytes());
             fos.close();
         } catch (Throwable t) {
             Crashlytics.logException(new Exception("Extra log exception"));
         }
     }
+
+    public static String readLocalLog() {
+        return localLog;
+    }
+
+    public static String copyLocalLogToClipBoard() {
+
+        ClipboardManager clipboard = (ClipboardManager) FetLifeApplication.getInstance().getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("FetLife Log", localLog);
+        clipboard.setPrimaryClip(clip);
+        
+        return localLog;
+    }
+
 }
